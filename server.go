ec982da26a1a1e46210a790765e633b6f5cbd19b
yeerkkiller1 <yeerkkiller1@gmail.com>
2015-08-26 18:56:51 -0400

    Need to use flag properly.

--- server.go
+++ server.go
@@ -8,14 +8,15 @@ import (
 )
 
 func main() {
-  port := *flag.Int("port", 7070, "The port the server will listen on.")
+  port := flag.Int("port", 7070, "The port the server will listen on.")
+  flag.Parse()
 
-  fmt.Println("Listening on port", port)
+  fmt.Println("Listening on port", *port)
 
   fs := http.FileServer(http.Dir("./"))
   http.Handle("/", fs)
 
-  err := http.ListenAndServe(":" + strconv.Itoa(port), nil)
+  err := http.ListenAndServe(":" + strconv.Itoa(*port), nil)
   if err != nil {
     fmt.Printf("error: %s\n", err)
   }
