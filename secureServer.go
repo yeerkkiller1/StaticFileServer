de9afc2fca173b5539fcc933b5ac66639f6e0136
yeerkkiller1 <yeerkkiller1@gmail.com>
2015-09-02 14:12:57 -0400

    What was I smoking when I wrote this?

--- secureServer.go
+++ secureServer.go
@@ -18,14 +18,14 @@ func main() {
   port := flag.Int("port", 7070, "The port the server will listen on.")
   flag.Parse()
 
-  fmt.Println("Listening on port", port*)
+  fmt.Println("Listening on port", *port)
 
   fs := http.FileServer(http.Dir("./"))
   http.Handle("/", fs)
 
   //Can't listen on same port for both, because the port is in use?
   //http.ListenAndServe(":7071", nil)
-  err := http.ListenAndServeTLS(":" + strconv.Itoa(port*), CERT, KEY, nil)
+  err := http.ListenAndServeTLS(":" + strconv.Itoa(*port), CERT, KEY, nil)
   if err != nil {
     fmt.Printf("SSL error: %s\n", err)
   }
